8567272fbc1f2df7e1faafb00ab05e8b5a700c8b
Sergey Kulanov <skulanov@mirantis.com>
2016-11-16 17:46:58 +0200

    Update getGitDescribe function
    Parametrize getGitDescribe(Boolean useShort = false):
      useShort Boolean, returns String in format:
           false (Default): {gitTag}-{numCommits}-g{gitsha}
           true:            {gitTag}-{numCommits}
    
    Change-Id: Ibf2e90f2fdef0611c3622b817d5f4e174fa90200

--- src/com/mirantis/mcp/Git.groovy
+++ src/com/mirantis/mcp/Git.groovy
@@ -13,11 +13,23 @@ def getGitCommit() {
 
 /**
  * Describe a commit using the most recent tag reachable from it
+ *
+ * @param useShort Boolean, which String format returns as result.
+ *              false (Default): {gitTag}-{numCommits}-g{gitsha}
+ *              true:            {gitTag}-{numCommits}
  */
-def getGitDescribe() {
-    git_commit = sh (
-            script: 'git describe --tags',
-            returnStdout: true
-    ).trim()
+def getGitDescribe(Boolean useShort = false) {
+    if (useShort) {
+        // original sed "s/-g[0-9a-f]\+$//g" should be escaped in groovy
+        git_commit = sh (
+                script: 'git describe --tags | sed "s/-g[0-9a-f]\\+$//g"',
+                returnStdout: true
+        ).trim()
+    } else {
+        git_commit = sh (
+                script: 'git describe --tags',
+                returnStdout: true
+        ).trim()
+    }
     return git_commit
 }
