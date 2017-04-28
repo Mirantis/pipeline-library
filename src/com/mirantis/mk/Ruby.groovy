ad59b2faff4523468aed5a057928d0d050fd3109
Jakub Josef <jakub.josef@gmail.com>
2017-04-28 16:08:40 +0200

    Improved kitchen methods in Ruby file
    Change-Id: I7cdc5eebcceffcb212b77171d28629a7ebf809d8

--- src/com/mirantis/mk/Ruby.groovy
+++ src/com/mirantis/mk/Ruby.groovy
@@ -4,22 +4,37 @@ package com.mirantis.mk
  * Ruby functions
  */
 
-def ensureRubyEnv(rubyVersion="2.4.0"){
-    sh """rbenv install ${rubyVersion};
-          rbenv local ${rubyVersion};
-          rbenv exec gem update --system"""
+/**
+ * Ensures Ruby environment with given version (install it if necessary)
+ * @param rubyVersion target ruby version (optional, default 2.3.2)
+ */
+def ensureRubyEnv(rubyVersion="2.3.2"){
+    if(!fileExists("/var/lib/jenkins/.rbenv/versions/${rubyVersion}/bin/ruby")){
+        sh "rbenv install ${rubyVersion}";
+    }
+    sh "rbenv local ${rubyVersion};rbenv exec gem update --system"
 }
+
+/**
+ * Install kitchen tools
+ */
 def installKitchen(){
     sh """rbenv exec gem install bundler;
           rbenv exec gem install test-kitchen"""
 }
 
+/**
+ * Run kitchen tests in tests/integration
+ */
 def runKitchenTests(){
     runKitchenCommand("converge")
     runKitchenCommand("verify -t tests/integration")
 }
 
-
+/**
+ * Run kitchen command
+ * @param cmd kitchen command
+ */
 def runKitchenCommand(cmd){
     sh "rbenv exec bundler exec kitchen ${cmd}"
 }
