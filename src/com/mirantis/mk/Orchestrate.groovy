200550f935c83e97cb851a88e2790ff44389a745
Jakub Josef <jakub.josef@gmail.com>
2017-05-16 17:39:55 +0200

    Moved sync_all before linux formula in orchestating
    Change-Id: I2073c3c5a4296b9e4098304d4a0a5fa1dbbe26aa

--- src/com/mirantis/mk/Orchestrate.groovy
+++ src/com/mirantis/mk/Orchestrate.groovy
@@ -19,10 +19,10 @@ def installFoundationInfra(master) {
     def salt = new com.mirantis.mk.Salt()
 
     salt.enforceState(master, 'I@salt:master', ['salt.master', 'reclass'], true)
-    salt.enforceState(master, '*', ['linux.system'], true)
-    salt.enforceState(master, '*', ['salt.minion'], true)
     salt.runSaltProcessStep(master, 'I@linux:system', 'saltutil.refresh_pillar', [], null, true)
     salt.runSaltProcessStep(master, 'I@linux:system', 'saltutil.sync_all', [], null, true)
+    salt.enforceState(master, '*', ['linux.system'], true)
+    salt.enforceState(master, '*', ['salt.minion'], true)
     salt.enforceState(master, 'I@linux:system', ['linux', 'openssh', 'salt.minion', 'ntp'], true)
 }
 
