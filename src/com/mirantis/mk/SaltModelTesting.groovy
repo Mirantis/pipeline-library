316eefe9051e5253dc309fc6d5fc1f5a94d2dc51
chnyda <chnyda@mirantis.com>
2017-11-13 11:26:01 +0100

    Kill salt-master & minion after change in configuration
    Change-Id: I1c3883e3a756cd03b7a3950cbfa4d8147c524352

--- src/com/mirantis/mk/SaltModelTesting.groovy
+++ src/com/mirantis/mk/SaltModelTesting.groovy
@@ -58,8 +58,8 @@ def setupAndTestNode(masterName, clusterName, extraFormulas, testDir, formulasSo
           sh("bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && saltmaster_bootstrap'")
         } else {
           sh("cp -r ${testDir}/* /srv/salt/reclass && echo '127.0.1.2  salt' >> /etc/hosts")
-          sh("service salt-master restart && service salt-minion restart && sleep 15")
           sh("bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && configure_salt_master && configure_salt_minion'")
+          sh("(pgrep salt-master | sed /$$/d | xargs --no-run-if-empty -i{} kill -9 {} || true) && pkill -9 salt-minion || true")
           sh("service salt-master restart && service salt-minion restart && sleep 15")
         }
 
