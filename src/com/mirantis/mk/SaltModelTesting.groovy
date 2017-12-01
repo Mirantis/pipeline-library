c065302cfbba8d4ee379fcc9ed27d665935cad1e
Jakub Josef <jakub.josef@gmail.com>
2017-12-01 14:22:47 +0100

    Increase salt master bootstrap timeout to 40min, fixed xargs interpolation parameter
    Change-Id: I5ef089746d2513952da6d3d3c8ea68bc57980c19

--- src/com/mirantis/mk/SaltModelTesting.groovy
+++ src/com/mirantis/mk/SaltModelTesting.groovy
@@ -62,12 +62,12 @@ def setupAndTestNode(masterName, clusterName, extraFormulas, testDir, formulasSo
           sh("bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && saltmaster_bootstrap'")
         } else {
           sh("cp -r ${testDir}/* /srv/salt/reclass && echo '127.0.1.2  salt' >> /etc/hosts")
-          sh("timeout 1200 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && configure_salt_master && configure_salt_minion && install_salt_formula_pkg'")
-          sh("(pgrep salt-master | sed /\$\$/d | xargs --no-run-if-empty -i{} kill -9 {} || true) && pkill -9 salt-minion || true")
+          sh("timeout 2400 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && configure_salt_master && configure_salt_minion && install_salt_formula_pkg'")
+          sh("(pgrep salt-master | sed /\$\$/d | xargs --no-run-if-empty -I{} kill -9 {} || true) && pkill -9 salt-minion || true")
           sh("service salt-master restart && service salt-minion restart && sleep 15")
         }
 
-        sh("timeout 1200 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && saltmaster_init'")
+        sh("timeout 2400 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && saltmaster_init'")
 
         if (!legacyTestingMode) {
            sh("bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && verify_salt_minions'")
