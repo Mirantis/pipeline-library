2a902f0710f1e67113187ea7e7e17ccd485ab276
chnyda <chnyda@mirantis.com>
2017-12-05 14:10:17 +0100

    Use internal aptly when testing models and change timeout
    Change-Id: I07eef7eabf23623863d161b4435ab9568e488615

--- src/com/mirantis/mk/SaltModelTesting.groovy
+++ src/com/mirantis/mk/SaltModelTesting.groovy
@@ -58,16 +58,20 @@ def setupAndTestNode(masterName, clusterName, extraFormulas, testDir, formulasSo
             "APT_REPOSITORY_GPG=${aptRepoGPG}"]){
 
         if (!imageFound) {
+          sh("cd /srv/salt && find . -type f \\( -name '*.yml' -or -name '*.sh' \\) -exec sed -i 's/apt-mk.mirantis.com/apt.mirantis.net:8085/g' {} \\;")
+          sh("cd /srv/salt && find . -type f \\( -name '*.yml' -or -name '*.sh' \\) -exec sed -i 's/apt.mirantis.com/apt.mirantis.net:8085/g' {} \\;")
           sh("bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && system_config_master'")
           sh("bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && saltmaster_bootstrap'")
         } else {
           sh("cp -r ${testDir}/* /srv/salt/reclass && echo '127.0.1.2  salt' >> /etc/hosts")
-          sh("timeout 2400 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && configure_salt_master && configure_salt_minion && install_salt_formula_pkg'")
-          sh("(pgrep salt-master | sed /\$\$/d | xargs --no-run-if-empty -I{} kill -9 {} || true) && pkill -9 salt-minion || true")
+          sh("cd /srv/salt && find . -type f \\( -name '*.yml' -or -name '*.sh' \\) -exec sed -i 's/apt-mk.mirantis.com/apt.mirantis.net:8085/g' {} \\;")
+          sh("cd /srv/salt && find . -type f \\( -name '*.yml' -or -name '*.sh' \\) -exec sed -i 's/apt.mirantis.com/apt.mirantis.net:8085/g' {} \\;")
+          sh("timeout 1200 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && configure_salt_master && configure_salt_minion && install_salt_formula_pkg'")
+          sh("(pgrep salt-master | sed /\$\$/d | xargs --no-run-if-empty -I {} kill -9 {} || true) && pkill -9 salt-minion || true")
           sh("service salt-master restart && service salt-minion restart && sleep 15")
         }
 
-        sh("timeout 2400 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && saltmaster_init'")
+        sh("timeout 1200 bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && source_local_envs && saltmaster_init'")
 
         if (!legacyTestingMode) {
            sh("bash -c 'source /srv/salt/scripts/bootstrap.sh; cd /srv/salt/scripts && verify_salt_minions'")
