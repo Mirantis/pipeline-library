68f46d2bca136dd94a950eabe508097d56a20242
Jakub Josef <jakub.josef@gmail.com>
2018-05-11 21:34:55 +0200

    Decreased verbosity of rsync during test of salt models
    Change-Id: If41ba333bc3ccd86dc9a5c1e411412725b8f09a5

--- src/com/mirantis/mk/SaltModelTesting.groovy
+++ src/com/mirantis/mk/SaltModelTesting.groovy
@@ -42,7 +42,7 @@ def setupAndTestNode(masterName, clusterName, extraFormulas, testDir, formulasSo
             "RECLASS_VERSION=${reclassVersion}", "RECLASS_IGNORE_CLASS_NOTFOUND=${ignoreClassNotfound}", "APT_REPOSITORY=${aptRepoUrl}",
             "APT_REPOSITORY_GPG=${aptRepoGPG}"]){
         sh("git clone https://github.com/salt-formulas/salt-formulas-scripts /srv/salt/scripts")
-        sh("""rsync -avh ${testDir}/* /srv/salt/reclass && echo '127.0.1.2  salt' >> /etc/hosts
+        sh("""rsync -ah ${testDir}/* /srv/salt/reclass && echo '127.0.1.2  salt' >> /etc/hosts
               cd /srv/salt && find . -type f \\( -name '*.yml' -or -name '*.sh' \\) -exec sed -i 's/apt-mk.mirantis.com/apt.mirantis.net:8085/g' {} \\;
               cd /srv/salt && find . -type f \\( -name '*.yml' -or -name '*.sh' \\) -exec sed -i 's/apt.mirantis.com/apt.mirantis.net:8085/g' {} \\;""")
         sh("""for s in \$(python -c \"import site; print(' '.join(site.getsitepackages()))\"); do
