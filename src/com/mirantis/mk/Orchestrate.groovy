dde1214f00d6e5272ffc39137e98ad8b15d5cf77
Martin Polreich <polreichmartin@gmail.com>
2018-06-14 09:32:20 +0200

    Add retry for glusterfs.client state to try to avoid Stale file handle error
    Fixes: PROD-20530
    
    Change-Id: Ib6d1e57e6baf622b0125388b92757af2ae99f327

--- src/com/mirantis/mk/Orchestrate.groovy
+++ src/com/mirantis/mk/Orchestrate.groovy
@@ -169,7 +169,7 @@ def installInfra(master) {
 
     // Ensure glusterfs clusters is ready
     if (salt.testTarget(master, 'I@glusterfs:client')) {
-        salt.enforceState(master, 'I@glusterfs:client', 'glusterfs.client')
+        salt.enforceState(master, 'I@glusterfs:client', 'glusterfs.client', true, true, null, false, -1, 2)
     }
 
     // Install galera
