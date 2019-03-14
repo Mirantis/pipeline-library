cd17544344a885d951141230c8d370cbe469e38b
Denis Egorenko <degorenko@mirantis.com>
2019-03-14 19:09:50 +0400

    Add archivation for comparePillars func
    Related-Prod: PROD-28146 (PROD:28146)
    Change-Id: I889d5e7363e260242abfbd3eb002e70c85866446

--- src/com/mirantis/mk/Common.groovy
+++ src/com/mirantis/mk/Common.groovy
@@ -730,7 +730,9 @@ def comparePillars(compRoot, b_url, grepOpts) {
                     description += "<li><a href=\"${httpWS}/old/${item}/*view*/\">${item}</a></li>"
                 }
             }
-
+            def cwd = sh(script: 'basename $(pwd)', returnStdout: true)
+            sh "tar -cf old_${cwd}.tar.gz old/ && rm -rf old/"
+            sh "tar -cf new_${cwd}.tar.gz new/ && rm -rf new/"
         }
     }
 
