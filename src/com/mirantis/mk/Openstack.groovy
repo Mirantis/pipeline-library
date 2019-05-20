0a340f87a1c90dd9e3da5d7455c1118df5ac4761
vnaumov <vnaumov@mirantis.com>
2019-05-20 12:21:47 +0400

    [Openstack libs] hot fix for python2.7 venv
    To avoid failures for python 2.7 venv due
    `TypeError: 'encoding' is an invalid keyword argument for this function`
    reason: usptream warlock package release:
    
    https://github.com/bcwaldon/warlock/commit/4241a7a9fbccfce7eb3298c2abdf00ca2dede64a
    
    Prod-related: PROD-30416 (PROD:30416)
    
    Change-Id: I17ca18665596a76610773080738aa0cd794033b4

--- src/com/mirantis/mk/Openstack.groovy
+++ src/com/mirantis/mk/Openstack.groovy
@@ -49,11 +49,15 @@ def setupOpenstackVirtualenv(path, version = 'latest') {
         'cliff==2.8',
         // NOTE(vsaienko): cmd2 is dependency for cliff, since we don't using upper-contstraints
         // we have to pin cmd2 < 0.9.0 as later versions are not compatible with python2.
+        // the same for warlock package due: https://github.com/bcwaldon/warlock/commit/4241a7a9fbccfce7eb3298c2abdf00ca2dede64a
         // TODO(vsaienko): use upper-constraints here, as in requirements we set only lowest library
         //                 versions.
         'cmd2<0.9.0;python_version=="2.7"',
         'cmd2>=0.9.1;python_version=="3.4"',
         'cmd2>=0.9.1;python_version=="3.5"',
+        'warlock<=1.3.1;python_version=="2.7"',
+        'warlock>1.3.1;python_version=="3.4"',
+        'warlock>1.3.1;python_version=="3.5"',
         'python-openstackclient',
         'python-octaviaclient',
         'python-heatclient',
