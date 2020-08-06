ce52609fe1c739658f937fb727dcd1fa84909b5b
Ivan Udovichenko <iudovichenko@mirantis.com>
2020-08-07 00:39:53 +0300

    [docker-image-scanner] Increase the value of maxResults value
    We reached >50 issues with CVEs, and default max results
    value doesn't satisfy our needs anymore.
    
    Change-Id: Ie6ac53e7418d1913312451188671f6194c04dc24
    Related-PROD: https://mirantis.jira.com/browse/RE-274

--- src/com/mirantis/mk/DockerImageScanner.groovy
+++ src/com/mirantis/mk/DockerImageScanner.groovy
@@ -139,7 +139,7 @@ def reportJiraTickets(String reportFileContents, String jiraCredentialsID, Strin
 
     def search_json = """
 {
-    "jql": "reporter = ${jiraUserID} and (labels = cve and labels = security) and (status = 'To Do' or status = 'For Triage' or status = Open or status = 'In Progress' or status = New)"
+        "jql": "project in (ENGORC,PRODX) and reporter = ${jiraUserID} and (labels = cve and labels = security) and (status = 'To Do' or status = 'For Triage' or status = Open or status = 'In Progress' or status = New)", "maxResults":-1
 }
 """
 
