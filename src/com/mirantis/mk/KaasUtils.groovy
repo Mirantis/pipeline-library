f8f23fa9d408d288f9d2e54bde24e768d5c96a2e
Владислав Наумов <vnaumov@mirantis.com>
2021-04-01 16:57:52 +0200

    [feat][core]  child-offline trigger
    used to apply offline sg/network on child deployment
    
    PRODX-12750
    
    Change-Id: Id37d421dbad411f2e25479872a106831746eeaa1

--- src/com/mirantis/mk/KaasUtils.groovy
+++ src/com/mirantis/mk/KaasUtils.groovy
@@ -67,7 +67,8 @@ def checkDeploymentTestSuite() {
 
     // proxy customization
     def proxyConfig = [
-        mgmtOffline: env.OFFLINE_MGMT_CLUSTER ? env.OFFLINE_MGMT_CLUSTER.toBoolean() : false, // TODO(vnaumov) add additonal vars for regional/child cluster ops
+        mgmtOffline: env.OFFLINE_MGMT_CLUSTER ? env.OFFLINE_MGMT_CLUSTER.toBoolean() : false,
+        childOffline: env.OFFLINE_CHILD_CLUSTER ? env.OFFLINE_CHILD_CLUSTER.toBoolean() : false,
     ]
 
     // optional demo deployment customization
@@ -110,6 +111,10 @@ def checkDeploymentTestSuite() {
     }
     if (commitMsg ==~ /(?s).*\[child-cfm\].*/ || env.GERRIT_EVENT_COMMENT_TEXT ==~ /(?s).*child-cfm.*/) {
         runChildConformance = true
+        deployChild = true
+    }
+    if (commitMsg ==~ /(?s).*\[child-offline\].*/ || env.GERRIT_EVENT_COMMENT_TEXT ==~ /(?s).*child-offline.*/) {
+        proxyConfig['childOffline'] = true
         deployChild = true
     }
     if (commitMsg ==~ /(?s).*\[fetch.*binaries\].*/ || env.GERRIT_EVENT_COMMENT_TEXT ==~ /(?s).*fetch.*binaries.*/) {
@@ -177,7 +182,7 @@ def checkDeploymentTestSuite() {
             openstack:  (proxyConfig['mgmtOffline'] == true) ? 'public-ci' : 'internal-ci',
             vsphere:  (proxyConfig['mgmtOffline'] == true) ? 'public-ci' : 'internal-ci',
             aws: 'public-ci',
-        ], // TODO(vnaumov) add additonal vars for regional cluster cdn ops
+        ],
     ]
 
     // calculate weight of current demo run to manage lockable resources
@@ -447,6 +452,7 @@ def triggerPatchedComponentDemo(component, patchSpec = '', configurationFile = '
         string(name: 'SI_PIPELINES_REFSPEC', value: siRefspec.siPipelines),
         string(name: 'CUSTOM_RELEASE_PATCH_SPEC', value: patchSpec),
         booleanParam(name: 'OFFLINE_MGMT_CLUSTER', value: triggers.proxyConfig['mgmtOffline']),
+        booleanParam(name: 'OFFLINE_CHILD_CLUSTER', value: triggers.proxyConfig['childOffline']),
         booleanParam(name: 'SEED_MACOS', value: triggers.useMacOsSeedNode),
         booleanParam(name: 'UPGRADE_MGMT_CLUSTER', value: triggers.upgradeMgmtEnabled),
         booleanParam(name: 'RUN_UI_E2E', value: triggers.runUie2eEnabled),
