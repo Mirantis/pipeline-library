58855c1742a612d19e7e86d148d513d3afe777c2
Mikhail Morgoev <mmorgoev@mirantis.com>
2023-02-10 14:57:31 +0100

    add trigger to use vsphere vm templates
    PRODX-28210
    
    Change-Id: I66a9b2fb4821128c202ebb99e1d60890966e1043

--- src/com/mirantis/mk/KaasUtils.groovy
+++ src/com/mirantis/mk/KaasUtils.groovy
@@ -115,6 +115,7 @@ def checkDeploymentTestSuite() {
     def equinixMetalV2Metro = env.EQUINIX_MGMT_METRO ? env.EQUINIX_MGMT_METRO : ''
     def enableFips = env.ENABLE_FIPS ? env.ENABLE_FIPS.toBoolean() : false
     def aioCluster = env.AIO_CLUSTER ? env.AIO_CLUSTER.toBoolean() : false
+    def useVsphereVvmtObjects = env.VSPHERE_USE_VVMT_OBJECTS ? env.VSPHERE_USE_VVMT_OBJECTS.toBoolean() : false
 
     def commitMsg = env.GERRIT_CHANGE_COMMIT_MESSAGE ? new String(env.GERRIT_CHANGE_COMMIT_MESSAGE.decodeBase64()) : ''
     if (commitMsg ==~ /(?s).*\[mgmt-proxy\].*/ || env.GERRIT_EVENT_COMMENT_TEXT ==~ /(?s).*mgmt-proxy.*/) {
@@ -414,6 +415,10 @@ def checkDeploymentTestSuite() {
         enableFips = true
     }
 
+    if (commitMsg ==~ /(?s).*\[vsphere-vvmt-obj\].*/ || env.GERRIT_EVENT_COMMENT_TEXT ==~ /(?s).*vsphere-vvmt-obj\.*/) {
+        useVsphereVvmtObjects = true
+    }
+
     // parse equinixmetalv2-metro trigger
     def equinixMetalV2MetroMatcher = (commitMsg =~ /\[equinixmetalv2-metro(\s+.*)?\]/)
     if (equinixMetalV2OnDemandDemo && equinixMetalV2MetroMatcher.size() > 0) {
@@ -475,6 +480,7 @@ def checkDeploymentTestSuite() {
         FIPS enabled: ${enableFips}
         Pause for debug enabled: ${pauseForDebug}
         AIO cluster: ${aioCluster}
+        Use Vsphere VVMT Objects: ${useVsphereVvmtObjects}
         Triggers: https://gerrit.mcp.mirantis.com/plugins/gitiles/kaas/core/+/refs/heads/master/hack/ci-gerrit-keywords.md""")
     return [
         osCloudLocation                          : openstackIMC,
@@ -530,7 +536,8 @@ def checkDeploymentTestSuite() {
         bootstrapV2Scenario                      : bootstrapV2Scenario,
         equinixMetalV2Metro                      : equinixMetalV2Metro,
         enableFips                               : enableFips,
-        aioCluster                               : aioCluster]
+        aioCluster                               : aioCluster,
+        useVsphereVvmtObjects                    : useVsphereVvmtObjects]
 }
 
 /**
